a7920a37ec697c3aa89a9bffdc3ce8919240e42e
Deepansh Sachdeva <deepansh.99530@gmail.com>
2021-12-17 01:34:01 -0500

    :tada: firebase integrated

---
+++ info6350-final-project/Models/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  info6350-final-project
+//
+//  Created by Deepansh Sachdeva on 12/16/21.
+//
+
+import Foundation
---
+++ info6350-final-project/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  info6350-final-project
+//
+//  Created by Deepansh Sachdeva on 12/16/21.
+//
+
+import Foundation
